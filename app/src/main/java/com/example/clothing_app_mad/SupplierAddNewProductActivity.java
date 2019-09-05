e90aacd40a2eb2a3c0cf7ef17c93ba75277435a8
Kaushalya Madushani <mkaushalya96@gmail.com>
2019-09-05 16:19:42 +0530

    Adding NavDrawer

---
+++ app/src/main/java/com/example/clothing_app_mad/SupplierAddNewProductActivity.java
@@ -0,0 +1,14 @@
+package com.example.clothing_app_mad;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SupplierAddNewProductActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_supplier_add_new_product);
+    }
+}
