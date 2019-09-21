da85ef2b4ca3fb67e15390ceadf5c51b9486d437
Kaushalya Madushani <mkaushalya96@gmail.com>
2019-09-21 14:08:39 +0530

    retrive details to the update form and validate the update form

--- app/src/main/java/com/example/clothing_app_mad/Seller/SellerMaintainProductsActivity.java
+++ app/src/main/java/com/example/clothing_app_mad/Seller/SellerMaintainProductsActivity.java
@@ -1,13 +1,27 @@
 package com.example.clothing_app_mad.Seller;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.example.clothing_app_mad.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.squareup.picasso.Picasso;
+
+import java.util.HashMap;
 
 public class SellerMaintainProductsActivity extends AppCompatActivity {
 
@@ -15,10 +29,17 @@ public class SellerMaintainProductsActivity extends AppCompatActivity {
     private EditText name, price, description;
     private ImageView imageView;
 
+    private String productID = "";
+    private DatabaseReference productRef;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate( savedInstanceState );
         setContentView( R.layout.activity_seller_maintain_products );
+
+        //to retrive the data to the update form
+        productID = getIntent().getStringExtra("pid");
+        productRef = FirebaseDatabase.getInstance().getReference().child( "Product" ).child(productID);
 
         productUpdateBtn = findViewById(R.id.product_update_Btn);
         name = findViewById(R.id.product_name_update);
@@ -26,5 +47,85 @@ public class SellerMaintainProductsActivity extends AppCompatActivity {
         description = findViewById(R.id.product_description_update);
         imageView = findViewById(R.id.product_image_update);
 
+        displaySelectProductInfo();
+
+        productUpdateBtn.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                applyChanges();
+            }
+        } );
+    }
+
+    private void applyChanges(){
+
+        String pName = name.getText().toString();
+        String pPrice = price.getText().toString();
+        String pDescription = description.getText().toString();
+
+        if (pName.equals("")){
+
+            Toast.makeText( this,"Enter the product name...", Toast.LENGTH_LONG).show();
+        }
+        else if (pPrice.equals("")){
+
+            Toast.makeText( this,"Enter the product price...", Toast.LENGTH_LONG).show();
+        }
+        else if (pDescription.equals("")){
+
+            Toast.makeText( this,"Enter the product description...", Toast.LENGTH_LONG).show();
+        }
+        else {
+
+            HashMap<String, Object> productMap = new HashMap<>();
+            productMap.put("pid", productID);
+            productMap.put("pname", pName);
+            productMap.put("description", pDescription);
+            productMap.put("price", pPrice);
+
+            productRef.updateChildren(productMap).addOnCompleteListener( new OnCompleteListener<Void>() {
+                @Override
+                public void onComplete(@NonNull Task<Void> task) {
+
+                    if (task.isSuccessful()){
+
+                        Toast.makeText(SellerMaintainProductsActivity.this, "Product Details Updated Successfully...", Toast.LENGTH_LONG).show();
+
+                        Intent intent = new Intent(SellerMaintainProductsActivity.this, SellerCategoryActivity.class);
+                        startActivity(intent);
+                        finish();
+                    }
+                }
+            } );
+        }
+    }
+
+    //to retrive the data to the update form
+    private void displaySelectProductInfo(){
+
+        productRef.addValueEventListener( new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot)
+            {
+                if (dataSnapshot.exists()){
+
+                    String pName = dataSnapshot.child("pname").getValue().toString();
+                    String pPrice = dataSnapshot.child("price").getValue().toString();
+                    String pDescription = dataSnapshot.child("description").getValue().toString();
+                    String pImage = dataSnapshot.child("image").getValue().toString();
+
+                    name.setText(pName);
+                    price.setText(pPrice);
+                    description.setText(pDescription);
+                    Picasso.get().load(pImage).into(imageView);
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        } );
     }
 }
